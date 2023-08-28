9727c9320989514e221ed0d3f8dc9fce6f891552
Tran Hai Dang <lighthouselp95@gmail.com>
2023-08-28 17:17:36 +0700

    center full response number comment

--- publics/app.js
+++ publics/app.js
@@ -89,7 +89,7 @@ function addPosts(posts) {
             </div>
 
             <div class="comment-button">
-                <button class="button-8">Comment</button>
+                <button class="button-8" data-number-comment="${post.comments.length}">Comment</button>
             </div>
            
             <div class="comment-content">
