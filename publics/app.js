c32dd19aab2f83ef4c926b6fbf649309e1961a62
Tran Hai Dang <lighthouselp95@gmail.com>
2023-09-05 04:11:14 +0700

    convert cloudinary url to public_id4

--- publics/app.js
+++ publics/app.js
@@ -88,7 +88,7 @@ function addPosts(posts) {
             </div>
             <div class="post-body">${post.file? (post.file.length!==0? ('<div class="img-wrapper">'+(post.file.map((e) => e.resource_type=='image'? //post.file[0].url.split('.').pop()=='mp4'? 
             `<img src=${e.url} class="post-img" width="100%" height="100%">`
-            :  e.resource_type=='video'?`<video  class="post-img" width="100%" height="100%" controls autoplay="autoplay" muted><source src=${e.url}></video>`
+            :  e.resource_type=='video'?`<video  class="post-img" width="100%" height="100%" controls autoplay="autoplay" webkit-playsinline playsinline muted><source src=${e.url}></video>`
             :`<audio controls class="post-img" width="100%" height="100%" autoplay="autoplay">
                 <source src=${e.url}>
                 Your browser does not support the audio tag.
