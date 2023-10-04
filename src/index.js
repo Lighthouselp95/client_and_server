47fe8ff67df9ff6da5683ba72241ea7b2acb8305
Tran Hai Dang <lighthouselp95@gmail.com>
2023-10-04 13:25:46 +0700

    ipv4 pass 6

--- src/index.js
+++ src/index.js
@@ -49,10 +49,10 @@ app.use(morgan('dev'));
 // })
 // console.log(http.METHODS, http.STATUS_CODES);
 
-app.set('trust proxy', ['loopback', 'linklocal, 162.158.0.0/16', 'uniquelocal, 172.70.0.0/12'])
-// app.set('trust proxy', (ip) => {
-//     if (ip === '162.158.0.0/16' || ip === '172.70.0.0/12' ) return true // trusted IPs
-//   });
+app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
+app.set('trust proxy', (ip) => {
+    if (ip === '162.158.0.0/16' || ip === '172.70.0.0/12' ) return true // trusted IPs
+  });
 app.use((req, res, next) => {
     console.log(req.ips);
     next();
