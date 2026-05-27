1cda692bf4d15b2caab16e4e5fa618abcc36eda1
Lighthouselp95 <133657191+Lighthouselp95@users.noreply.github.com>
2026-05-27 09:15:21 +0700

    Update controller.deletePost.js

--- src/controllers/controller.deletePost.js
+++ src/controllers/controller.deletePost.js
@@ -15,53 +15,25 @@ module.exports = async (req, res, next) => {
         if(blog.file&&blog.file.length!==0) {
             for(let e of blog.file) {
                 console.log(!e.public_id, '--', e.url);
-                // if(!e.public_id) {
-                //     e.public_id = e.url.split('/').slice(-1)[0];
+                
                     console.log(e.public_id, typeof e.public_id);
-                //     cloudinary.uploader.destroy(e.public_id)
-                //         .then(result => {
-                //             console.log(result);
-                //         })
-                //         .catch(err => {
-                //             console.log(err);
-                //             return res.status(403).send(err)
-                //         });
-                //     console.log('deleted on cloudinary');
-                // }
-                // else {
-                    // e.public_id = e.public_id.split('/')[1];
                     console.log(e.public_id)
                 let result = await cloudinary.uploader.destroy(e.public_id, {resource_type: e.resource_type, type: 'upload'});
                 console.log(result.result);
-                // if(result.result=='not found') {return res.status(403).send(result)};
-                    // .then((result, error) => 
-                    //     {
-                    //         console.log(result.result);
-                    //         if(result.result=='not found') {return res.status(403).send(result)};
-                            
-                    //     }
-                    //     )
-                console.log('deleted on cloudinary');
-                // }
-                
+                console.log('deleted on cloudinary');  
             }
-                // result = await Blog.findByIdAndRemove(`${req.params.id}`).exec();
-                // console.log(result);
-                // return res.status(200).send('deleted');
+             
         }
-        //  else {
                 await Blog.findByIdAndRemove(`${req.params.id}`).exec();
                 return res.status(200).send('deleted');
-        // }
+
     } else 
         {
             console.log('sai');
             return res.status(401).send('Unauthorized');
-        
         }
 } catch (err) {
     console.log(err);
     return res.status(403).send(err);
-
 };
 }
